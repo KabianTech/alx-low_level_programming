a3edcc774dbd7081a85f53d5cd1e5b5dd933b10d
KabianTech <kabiantechzone@gmail.com>
2023-10-09 16:17:12 -0700

    print comb3

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+/**
+ * main - main function
+ *
+ *Return: always 0
+ */
+
+int main(void)
+{
+	int i = '0';
+	int x = '0';
+
+	while (i <= '9')
+	{
+		while (x <= '9')
+		{
+			if (!(i > x) || i == x)
+			{
+				putchar(i);
+				putchar(x);
+				if (i == '8' && x == '9')
+				{
+					putchar('\n');
+				}
+				else
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+			x++;
+		}
+		x = '0';
+		i++;
+	}
+	return (0);
+}
