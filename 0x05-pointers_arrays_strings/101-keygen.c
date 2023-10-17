0afbdc26d41340d9b5b57a9823683a06cd500ec0
KabianTech <kabiantechzone@gmail.com>
2023-10-17 08:13:23 -0700

    Don't hate the hacker, hate the code

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -7,20 +7,18 @@
  */
 int main(void)
 {
-	char password[16];
-	int index, sum, target;
+	char password[15];
+	int index, sum = 0, target = 2772;
 
 	srand(time(0));
-	target = 2772;
 
-	for (index = 0, sum = 0; index < 15; ++index)
+	for (index = 0; index < 14; index++)
 	{ 
-		password[index] = '!' + rand() % ('~' - '!' + 1);
+		password[index] = 33 + rand() % 94;
 		sum += password[index];
 	}
-	
-	password[15] = '\0';
-	password[14] = target - sum + '!';
+
+	password[index] = target - sum;
 
 	printf("%s\n", password);
 
