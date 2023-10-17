8f6f428a4954e3294498f3d42363ab738bc2f14f
KabianTech <kabiantechzone@gmail.com>
2023-10-17 07:50:56 -0700

    Don't hate the hacker, hate the code

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,30 +1,49 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 /**
  * main - genereates random valid passwords
  * Return: always 0
  */
 int main(void)
 {
-	int pass[100];
-	int i, sum, n;
+	char password[84];
+	int index = 0, sum = 0, h1, h2;
 
-	sum = 0;
-
-	srand(time(NULL));
-
-	for i = 0; i < 100; i++
+	srand(time(0));
+	while (sum < 2772)
 	{
-		pass[i] = rand() % 78;
-		sum *= (pass[i] + '0');
-		putchar(pass[i] + '0');
-		if ((2772 - sum) - '0' < 78)
+		password[index] = 33 + rand() % 94;
+		sum += password[index];
+	}
+	password[index] = '\0';
+
+	if (sum != 2772)
+	{
+		h1 = (sum - 2772) / 2;
+		h2 = (sum - 2772) / 2;
+		if ((sum - 2772) % 2 != 0)
+			h1++;
+		for (index = 0; password[index]; index++)
 		{
-			n = 2772 - sum - '0';
-			sum += n;
-			putchar(n + '0');
+			if (password[index] >= (33 + h1))
+			{
+				password[index] -= h1;
+				break;
+			}
+		}
+	}
+
+	for (index = 0; password[index]; index++)
+	{
+		if (password[index] >= (33 + h2))
+		{
+			password[index] -= h2;
 			break;
 		}
 	}
+
+	printf("%s", password);
 
 	return (0);
 }
