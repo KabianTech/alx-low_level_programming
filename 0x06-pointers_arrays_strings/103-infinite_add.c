21450caf77dc1e800740a5f38e3d00e710d0b2db
KabianTech <kabiantechzone@gmail.com>
2023-10-20 14:29:09 -0700

    Numbers have life; they're not just symbols on paper

---
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * infinite_add - adds two numbers
+ * @n1: first number
+ * @n2: second number
+ * r: buffer that the function will use to store the result
+ * size_r: buffer size
+ * Return: pointer to the result
+ */
+char *infinite_add(char *n1, char *n2, char *r, int size_r)
+{
+	
