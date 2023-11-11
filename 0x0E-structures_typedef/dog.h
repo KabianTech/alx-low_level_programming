115a9e3cec7f291ed9c79073983793730df150e3
KabianTech <kabiantechzone@gmail.com>
2023-11-11 07:51:03 -0800

    Poppy

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - a new type describing a dog
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+/**
+ * dog_t - Typedef for struct dog
+ */
+typedef struct dog dog_t;
+
+#endif
