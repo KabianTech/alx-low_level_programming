b262d404dc3c90e138ab11ccb88f5648143c3c3a
KabianTech <kabataian01@gmail.com>
2023-11-17 16:31:49 -0800

    print list

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -6,20 +6,20 @@
  */
 size_t print_list(const list_t *h)
 {
-	size_t nodes;
+	int nodes;
 
-	nodes = 0;
+	if (h == NULL)
+		return (0);
 
-	while  (h != NULL)
+	for (nodes = 1; h->next != NULL; nodes++)
 	{
 		if (h->str == NULL)
-			printf("[%d] %s\n", 0, "(nil)");
+			printf("[%u] %s\n", h->len, "(nil)");
 		else
-			printf("[%d] %s\n", h->len, h->str);
-
+			printf("[%u] %s\n", h->len, h->str);
 		h = h->next;
-		nodes++;
 	}
+	printf("[%u] %s\n", h->len, h->str);
 
 	return (nodes);
 }
