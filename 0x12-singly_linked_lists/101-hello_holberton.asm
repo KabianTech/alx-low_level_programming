bf41b26ce41d6482b1ef3891dd1376ff58e84660
KabianTech <kabataian01@gmail.com>
2023-11-17 17:00:49 -0800

    64-bit program in assembly language

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,9 +1,11 @@
 global  main
 	extern printf
+section .data
+	format db 'Hello, Holberton', 0xA, 0
+section .text
 main:
 	mov edi, format
-	xor eax, eax
+	mov eax, 0
 	call printf
-	mov 	eax, 0
+	mov eax, 0
 	ret
-format: db 'Hello, Holberton', 0
